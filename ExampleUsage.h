4ae263e049013bacc844958b19310ef1a6f981c8
MiddleRaster <235772580+MiddleRaster@users.noreply.github.com>
2025-10-24 12:16:49 -0400

    mocking an automatic variable in a static method
    Refactor code to introduce templated production code structure.

--- ExampleUsage.h
+++ ExampleUsage.h
@@ -1,6 +1,41 @@
+#pragma once
 #ifndef EXAMPLEUSAGE_H
 #define EXAMPLEUSAGE_H
 
-// still working on this.
+// the dependency
+struct IWantToMockThis
+{
+    bool ImagineThisIsReallySlow() { return true; }
+};
+
+/* the original production code before TBCI
+struct ProductionCode
+{
+    static bool Foo()
+    {
+        IWantToMockThis obj;
+        return obj.ImagineThisIsReallySlow();
+    }
+};
+*/
+
+struct Empty
+{
+    using IWantToMockThis = ::IWantToMockThis;
+};
+
+template<typename Base>
+struct ProductionCodeT : private Base
+{
+    using typename Base::IWantToMockThis;
+
+    static bool Foo()
+    {
+        IWantToMockThis obj;
+        return obj.ImagineThisIsReallySlow();
+    }
+};
+
+using ProductionCode = ProductionCodeT<Empty>; // so all existing client code still compiles
 
 #endif
