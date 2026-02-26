42c4494223f44158f2eacf50fb139f2e19031121
MiddleRaster <235772580+MiddleRaster@users.noreply.github.com>
2026-02-26 11:07:34 -0500

    Callable can forward to any function, not just in the global namespace

--- MockableFunction.h
+++ MockableFunction.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #define MOCKABLE_FUNCTION(NAME) template <typename... Args> static inline decltype(auto) NAME(Args&&... args) { return ::NAME(std::forward<Args>(args)...); }
+
+template<auto Fn>
+struct Callable
+{
+    template<typename... Args> decltype(auto) operator()(Args&&... args) const { return Fn(std::forward<Args>(args)...); }
+};
